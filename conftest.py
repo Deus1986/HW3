58dfc70a1c4fd91d0cefbe646b09cb8670a4f9ff
Vladimir Krinitsyn <Premio1896@rambler.ru>
2024-09-24 22:50:22 +0300

    Search google

--- conftest.py
+++ conftest.py
@@ -1,15 +1,13 @@
 import pytest
+from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-@pytest.fixture()
-def browser():
-    options = Options()
-    driver_service = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=driver_service, options=options)
-    driver.set_window_size(1600, 900)
-    yield driver
-    driver.quit()
+@pytest.fixture(scope="session")
+def set_browser_window_size():
+    browser.config.driver_name = "firefox"
+    browser.config.window_height = 1024
+    browser.config.window_width = 768
--- google_should_find_selene.py
+++ google_should_find_selene.py
@@ -1,10 +1,17 @@
+import datetime
+import time
+
 from selene import browser, be, have
 
-browser.config.driver_name = 'firefox'
 
-
-def test_google_search():
-    browser.open('https://google.com')
+def test_negative_google_search(set_browser_window_size):
+    browser.open('https://www.google.ru/')
     search_value = "@#$$#%#@$@#@%"
     browser.element('[name="q"]').should(be.blank).type(search_value).press_enter()
     browser.element('[id="botstuff"]').should(have.text(f'По запросу {search_value} ничего не найдено.'))
+
+
+def test_google_search(set_browser_window_size):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
